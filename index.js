8eaeccc41939d7a522b049b49a317e035542d2e2
Nghiep <me@nghiep.dev>
2022-12-22 11:14:24 +0700

    Implement cron

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
+require('dotenv').config();
 const cron = require('node-cron');
 
-cron.schedule('* * * * * *', () => {
-  console.log('running a task every minute', new Date().toLocaleString());
-});
+function validateUrl(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+const jobUrls = process.env.CRON_URLS.split('\n')
+  .map(urlJob => {
+    const [expression] = urlJob.match(/^.*(?=\shttp)/) ?? [];
+    const [url] = urlJob.match(/http.*/) ?? [];
+    return {
+      expression,
+      url,
+    };
+  })
+  .filter(({expression, url}) => {
+    return cron.validate(expression) && validateUrl(url);
+  });
+
+for (const {expression, url} of jobUrls) {
+  cron.schedule(
+    expression,
+    () => {
+      fetch(url);
+      console.info(
+        `${new Date().toLocaleString()} - ${expression} - Fetching ${url}`
+      );
+    },
+    {
+      timezone: process.env.TZ ?? 'Asia/Ho_Chi_Minh',
+    }
+  );
+}
